95b72d19f88da66109f32638b90f0319acf7692e
Max <maxvanliempdt@gmail.com>
2023-01-15 15:04:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function naarPagina1(){
+    const element = document.getElementById("pagina1");
+    element.scrollIntoView();
+}
+
+function naarPagina2(){
+    const element = document.getElementById("pagina2");
+    element.scrollIntoView({ behavior: "smooth" });
+}
+
+function naarPagina3(){
+    const element = document.getElementById("pagina3");
+    element.scrollIntoView({ behavior: "smooth" });
+}
+
+function naarPagina4(){
+    const element = document.getElementById("pagina4");
+    element.scrollIntoView({ behavior: "smooth" });
+}
+
+function naarPagina5(){
+    const element = document.getElementById("pagina5");
+    element.scrollIntoView({ behavior: "smooth" });
+}
+
+function naarPagina6(){
+    const element = document.getElementById("pagina6");
+    element.scrollIntoView({ behavior: "smooth" });
+}
+
+function naarPagina7(){
+    const element = document.getElementById("pagina7");
+    element.scrollIntoView({ behavior: "smooth" });
+}
